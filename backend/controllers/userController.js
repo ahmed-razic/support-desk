b15a3673d8d75a618915226908c4690140701beb
ahmed-razic <ahmed.razic@gmail.com>
2022-03-16 18:30:34 +0100

    errorHandler

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,7 +3,7 @@ const asyncHandler = require('express-async-handler');
 // @desc    Register a new user
 // @route   /api/users
 // @access  Public
-const registerUser =  asyncHandler(async (req, res) => {
+const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
   if (!name || !email || !password) {
     res.status(400);
@@ -11,16 +11,16 @@ const registerUser =  asyncHandler(async (req, res) => {
   }
 
   res.send('Register route');
-};)
+});
 
 // @desc    Login user
 // @route   /api/users/login
 // @access  Public
 const loginUser = asyncHandler(async (req, res) => {
   res.send('Login route');
-};
+});
 
 module.exports = {
   registerUser,
   loginUser,
-};)
+};
