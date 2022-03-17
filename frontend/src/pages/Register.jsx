e209edaeee9ebcab6084693306f5ed2a60946a87
ahmed-razic <ahmed.razic@gmail.com>
2022-03-17 18:20:36 +0100

    pages, header

---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+
+function Register() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: '',
+  });
+
+  const { name, email, password, password2 } = formData;
+
+  return <div>Register</div>;
+}
+export default Register;
